9ed3c8a6ef38c7f8f59297200c955c557808c8de
SHMAN12345 <shong-wm20@student.tarc.edu.my>
2023-04-10 00:00:28 +0800

    SS

---
+++ app/src/main/java/com/example/quickhiretest/ui/Profile/EditProfileFragment.kt
@@ -0,0 +1,43 @@
+package com.example.quickhiretest.ui.Profile
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import com.example.quickhiretest.R
+import com.example.quickhiretest.databinding.FragmentEditProfileBinding
+import com.example.quickhiretest.databinding.FragmentProfileBinding
+
+
+class EditProfileFragment : Fragment() {
+    private var _binding: FragmentEditProfileBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val dashboardViewModel =
+            ViewModelProvider(this).get(ProfileViewModel::class.java)
+
+        _binding = FragmentEditProfileBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+//        val textView: TextView = binding.textDashboard
+//        dashboardViewModel.text.observe(viewLifecycleOwner) {
+//            textView.text = it
+//        }
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
